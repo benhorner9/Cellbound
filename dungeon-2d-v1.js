e3390b4d2e80eebdfc2c5cdd1ce1febb224edafc
benhorner9 <benhorner9@googlemail.com>
2026-09-20 10:50:25 +0100

    Align dungeon recovery with level-framework combat bundle

--- dungeon-2d-v1.js
+++ dungeon-2d-v1.js
@@ -1083,7 +1083,7 @@ function ensureCombatRebornEngine(){
      else reject(error||new Error('Combat Reborn engine failed to initialise'))
    };
    const script=document.createElement('script');
-   script.src='./combat-identities-v1.js?v=12&recover=1';
+   script.src='./combat-identities-v1.js?v=13&recover=1';
    script.async=true;
    script.dataset.combatRebornRecovery='1';
    script.onload=()=>finish(true);
