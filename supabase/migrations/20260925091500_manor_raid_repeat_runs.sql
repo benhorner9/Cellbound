67a1e6c63c13a74a5808b164489418e6ef2c3f31
benhorner9 <benhorner9@googlemail.com>
2026-09-25 11:32:31 +0100

    Remove superseded Manor migration timestamp

--- supabase/migrations/20260925091500_manor_raid_repeat_runs.sql
+++
@@ -1,9 +0,0 @@
-alter table public.raid_sessions
-  drop constraint if exists raid_sessions_listing_id_key;
-
-create unique index if not exists raid_sessions_one_active_per_listing_idx
-  on public.raid_sessions(listing_id)
-  where status='active';
-
-create index if not exists raid_sessions_listing_started_idx
-  on public.raid_sessions(listing_id,started_at desc);
